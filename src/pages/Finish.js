91d6346a0a34aa4f549f158620a0c5dc61650889
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-06-22 14:59:58 +0300

    added quiz mechanics and result output

--- src/pages/Finish.js
+++ src/pages/Finish.js
@@ -1,5 +1,33 @@
-function Finish() {
-  return <div className="finish">Finish</div>;
-}
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const Finish = () => {
+  const location = useLocation();
+  const { quizData, userAnswers } = location.state;
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const calculateScore = () => {
+      let count = 0;
+      for (let i = 0; i < quizData.length; i++) {
+        if (userAnswers[i] === quizData[i].correct_answer) {
+          count++;
+        }
+      }
+      setScore(count);
+    };
+
+    calculateScore();
+  }, [quizData, userAnswers]);
+
+  return (
+    <div>
+      <h2>Результати квізу</h2>
+      <p>
+        Ваш результат: {score} з {quizData.length}
+      </p>
+    </div>
+  );
+};
 
 export default Finish;
--- src/pages/Play.js
+++ src/pages/Play.js
@@ -4,14 +4,50 @@ import { useLocation, useNavigate } from "react-router-dom";
 const Play = () => {
   const location = useLocation();
   const quizData = location.state;
-  console.log(quizData);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+  const navigate = useNavigate();
 
+  const currentQuestion = quizData[currentQuestionIndex];
+
+  const handleAnswerSelect = (selectedAnswer) => {
+    setUserAnswers((prevAnswers) => {
+      const updatedUserAnswers = [...prevAnswers];
+      updatedUserAnswers[currentQuestionIndex] = selectedAnswer;
+      return updatedUserAnswers;
+    });
+
+    if (currentQuestionIndex < quizData.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      navigate("/finish", {
+        state: { quizData, userAnswers: [...userAnswers, selectedAnswer] },
+      });
+    }
+  };
+
+  console.log(currentQuestion.correct_answer);
   return (
     <div>
       <h2>Квіз</h2>
-      {quizData[0].category}
+      {currentQuestion && (
+        <>
+          <h3>{currentQuestion.category}</h3>
+          <p>{currentQuestion.question}</p>
+          <ul>
+            {currentQuestion.incorrect_answers.map((answer, index) => (
+              <li key={index} onClick={() => handleAnswerSelect(answer)}>
+                {answer}
+              </li>
+            ))}
+            <li
+              onClick={() => handleAnswerSelect(currentQuestion.correct_answer)}
+            >
+              {currentQuestion.correct_answer}
+            </li>
+          </ul>
+        </>
+      )}
     </div>
   );
 };
