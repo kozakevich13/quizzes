8108a706bc55d0f62a9a76a11283e7e8977ce26a
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-06-22 13:29:15 +0300

    add 3 quizzes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">quizzes</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,37 @@
-function Home() {
-  return <div className="home">Home</div>;
-}
+import React, { useState } from "react";
+
+const Home = () => {
+  const [quizzes] = useState([
+    { quiz: "General Knowledge", questionCount: 5, category: 9 },
+    { quiz: "Entertainment: Books", questionCount: 8, category: 10 },
+    { quiz: "Entertainment: Film", questionCount: 10, category: 11 },
+  ]);
+
+  const handlePlayQuiz = async (quiz) => {
+    let amount = quiz.questionCount;
+    let category = quiz.category;
+    try {
+      const response = await fetch(
+        `https://opentdb.com/api.php?amount=${amount}&category=${category}&type=multiple`
+      );
+      const data = await response.json();
+      console.log("Quiz data:", data);
+    } catch (error) {
+      console.error("Error fetching quiz data:", error);
+    }
+  };
+  return (
+    <div>
+      <h2>Список квізів</h2>
+      {quizzes.map((quiz, index) => (
+        <div key={index}>
+          <h3>{quiz.quiz}</h3>
+          <p>Кількість питань: {quiz.questionCount}</p>
+          <button onClick={() => handlePlayQuiz(quiz)}>Грати</button>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Home;
