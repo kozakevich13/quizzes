248a25a7e081a2a8d1331c7b7e6badb4d06086db
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-06-22 21:10:09 +0300

    add quizCount

--- src/pages/Finish.js
+++ src/pages/Finish.js
@@ -6,6 +6,9 @@ const Finish = () => {
   const { quizData, userAnswers, timeTaken } = location.state;
   const [points, setPoints] = useState(0);
   const [totalPoints, setTotalPoints] = useState(0);
+  const [quizCount, setQuizCount] = useState(
+    parseInt(localStorage.getItem("quizCount")) || 0
+  );
 
   useEffect(() => {
     const calculateScore = () => {
@@ -30,6 +33,12 @@ const Finish = () => {
     setTotalPoints(updatedPoints);
   }, [points]);
 
+  useEffect(() => {
+    const updatedQuizCount = quizCount + 1;
+    localStorage.setItem("quizCount", updatedQuizCount.toString());
+    setQuizCount(updatedQuizCount);
+  }, []);
+
   return (
     <div className="user-result">
       <h2>Result</h2>
@@ -47,6 +56,8 @@ const Finish = () => {
       <p>Your time: {timeTaken} sec</p>
       <p>Your points: +{points}</p>
       <p>Total points: {totalPoints}</p>
+
+      <p>Total quizzes taken: {localStorage.getItem("quizCount")}</p>
     </div>
   );
 };
